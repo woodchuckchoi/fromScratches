163e8d602b886c80a0e58f4b3c2c54728cc074ca
woodchuckchoi <choiggoggi@gmail.com>
2020-08-10 18:51:31 +0900

    redis session added

--- back/user.py
+++ back/user.py
@@ -1,19 +1,33 @@
 # Built-in
 import os
+import time
 
 # PIP
+import aioredis
 from sanic import Sanic
 from sanic.response import json
+from sanic_session import Session, AIORedisSessionInterface
 
 # Custom
 from samaria import Samaria
 from utils import importConfig
 
 if __name__ == "__main__":
-    HOST, USER, PASS, DATABASE = importConfig()
+    HOST, USER, PASS, DATABASE, REDIS_HOST, REDIS_PORT = importConfig()
 
-    app     = Sanic(__name__)
-    samaria = Samaria(host=HOST, user=USER, password=PASS, database=DATABASE)
+    app             = Sanic(__name__)
+    samaria         = Samaria(host=HOST, user=USER, password=PASS, database=DATABASE)
+    redisSession    = Session()
+
+    @app.listener('before_server_start')
+    async def redis_init(app, loop):
+        app.redis   = await aioredis.create_redis_pool((REDIS_HOST, REDIS_PORT))
+        redisSession.init_app(app, interface=AIORedisSessionInterface(app.redis, expiry=21600, sessioncookie=True))
+
+    def check_auth(request):
+        if request.ctx.session.get('devQueueId') and request.ctx.session.get('devQueueLastLogin'):
+            return True
+        return False
 
     @app.route('/')
     async def all_user(request):
@@ -23,13 +37,21 @@ if __name__ == "__main__":
 
     @app.route('/login', methods=['POST'])
     async def login(request):
+        if check_auth(request):
+            return json({"result": True})
+
         loginId = request.json['loginId']
         loginPw = request.json['loginPw']
 
         await samaria.execute("SELECT IF(SHA2(CONCAT(creation_date, '{}'), 512) = password, 1, 0) FROM User WHERE id = '{}'".format(loginPw, loginId))
         
         ret = await samaria.fetchall()
+
+        # Login log collection logic to be added here
+
         if ret[0][0]:
+            request.ctx.session['devQueueId'] = loginId
+            request.ctx.session['devQueueLastLogin'] = time.strftime("%Y/%m/%d %H:%M:%S")
             return json({"result": True})
         return json({"result": False})
 
--- back/utils.py
+++ back/utils.py
@@ -11,5 +11,7 @@ def importConfig():
     USER    = os.environ['DB_USER'] if 'DB_USER' in os.environ else fallbackConfig['DB_USER']
     PASS    = os.environ['DB_PASS'] if 'DB_PASS' in os.environ else fallbackConfig['DB_PASS']
     DATABASE= os.environ['DB_DATABASE'] if 'DB_DATABASE' in os.environ else fallbackConfig['DB_DATABASE']
+    REDIS_H = os.environ['REDIS_HOST'] if 'REDIS_HOST' in os.environ else fallbackConfig['REDIS_HOST']
+    REDIS_P = os.environ['REDIS_PORT'] if 'REDIS_PORT' in os.environ else fallbackConfig['REDIS_PORT']
 
-    return HOST, USER, PASS, DATABASE
+    return HOST, USER, PASS, DATABASE, REDIS_H, REDIS_P
